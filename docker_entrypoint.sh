30cd8ac9de402f76ad54f9e15efb2f4b325f14ca
Aiden McClelland <me@drbonez.dev>
2023-09-17 16:32:02 -0600

    set model url based on available mem in entrypoint

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -2,4 +2,14 @@
 
 printf "\n\nStarting FreeGPT\n\n"
 
+total_memory_gb=$(awk '/MemAvailable/{print int($2 / 1024 / 1024)}' /proc/meminfo)
+
+if [ "$total_memory_gb" -gt 16 ]; then
+    export PUBLIC_MODELS_URL="https://raw.githubusercontent.com/Start9Labs/freegpt-startos/weights/models-mega.json"
+elif [ "$total_memory_gb" -gt 8 ]; then
+    export PUBLIC_MODELS_URL="https://raw.githubusercontent.com/Start9Labs/freegpt-startos/weights/models-big.json"
+else
+    export PUBLIC_MODELS_URL="https://raw.githubusercontent.com/Start9Labs/freegpt-startos/weights/models-small.json"
+fi
+
 exec /usr/src/app/deploy.sh
