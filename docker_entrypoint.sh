c0227d1935c351f47fd41d7bd60abe5aa22b5261
Aiden McClelland <me@drbonez.dev>
2023-09-17 23:23:24 -0600

    gut health check

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -5,11 +5,23 @@ printf "\n\nStarting FreeGPT\n\n"
 total_memory_gb=$(awk '/MemAvailable/{print int($2 / 1024 / 1024)}' /proc/meminfo)
 
 if [ "$total_memory_gb" -gt 16 ]; then
-    export PUBLIC_MODELS_URL="https://raw.githubusercontent.com/Start9Labs/freegpt-startos/weights/models-mega.json"
-elif [ "$total_memory_gb" -gt 8 ]; then
     export PUBLIC_MODELS_URL="https://raw.githubusercontent.com/Start9Labs/freegpt-startos/weights/models-big.json"
+elif [ "$total_memory_gb" -gt 8 ]; then
+    export PUBLIC_MODELS_URL="https://raw.githubusercontent.com/Start9Labs/freegpt-startos/weights/models-medium.json"
 else
     export PUBLIC_MODELS_URL="https://raw.githubusercontent.com/Start9Labs/freegpt-startos/weights/models-small.json"
 fi
+
+function initialize-models () {
+    while ! curl -sf http://localhost:8008/api/ping/ > /dev/null 2>&1; do
+        sleep 5
+    done
+    
+    curl -X POST "http://localhost:8008/api/model/refresh" \
+         -H "Content-Type: multipart/form-data" \
+         -F "url=$PUBLIC_MODELS_URL"
+}
+
+initialize-models &
 
 exec /usr/src/app/deploy.sh
--- health.sh
+++ health.sh
@@ -20,35 +20,3 @@ if [ "$exit_code" -ne 0 ]; then
         exit 1
     fi
 fi
-
-if [ -f /usr/src/app/curl.txt ]; then
-    echo "Cool" > /dev/null
-else
-    while ! curl -sf http://localhost:8008/api/ping/ >/dev/null 2>&1; do
-        sleep 5
-    done
-
-    total_memory_gb=$(awk '/MemAvailable/{print int($2 / 1024 / 1024)}' /proc/meminfo)
-
-    if [ "$total_memory_gb" -gt 8 ]; then
-        new_url="https://raw.githubusercontent.com/Start9Labs/freegpt-startos/weights/models-big.json"
-    else
-        new_url="https://raw.githubusercontent.com/Start9Labs/freegpt-startos/weights/models-small.json"
-    fi
-
-    curl -X POST "http://localhost:8008/api/model/refresh" \
-         -H "Content-Type: multipart/form-data" \
-         -F "url=$new_url"
-
-    echo "$(date) - The curl command was executed" >> /usr/src/app/curl.txt
-    sed -i "s#https://raw.githubusercontent.com/serge-chat/serge/main/api/src/serge/data/models.json#$new_url#" /usr/src/app/api/static/_app/immutable/nodes/4.*.js
-    #sed -i "s#col-span-3 flex flex-col#col-span-3 flex flex-col hidden#" /usr/src/app/api/static/_app/immutable/nodes/2.*.js
-    sed -i "s#Say Hi to Serge#Welcome to FreeGPT#" /usr/src/app/api/static/_app/immutable/nodes/2.*.js
-    #sed -i "s#px-10 pt-2 pb-4 text-center font-light md:px-16#px-10 pt-2 pb-4 text-center font-light md:px-16 hidden#" /usr/src/app/api/static/_app/immutable/nodes/3.*.js
-    sed -i "s#Message Serge#Message#" /usr/src/app/api/static/_app/immutable/nodes/3.*.js
-    p_remove="Below is an instruction that describes a task. Write a response that appropriately completes the request."
-    sed -i "s#$p_remove##" /usr/src/app/api/static/_app/immutable/nodes/2.*.js
-    sed -i "s#7B requires about 4.5GB of free RAM, 13B requires about 12GB free, 30B requires##" /usr/src/app/api/static/_app/immutable/nodes/4.*.js
-    sed -i "s#about 20GB free##" /usr/src/app/api/static/_app/immutable/nodes/4.*.js
-
-fi
